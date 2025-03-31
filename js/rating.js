8352785b2b5297497fe01e428088f00460d36025
irlknn <marta.muzyka.oi.2023@lpnu.ua>
2025-03-31 19:38:47 +0300

    commit

--- js/rating.js
+++ js/rating.js
@@ -6,25 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const leaderboardData = {
         // винести звідси
         today: [
-            { name: "Charles John", points: 195, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_1.png?raw=true" },
-            { name: "Alex Mike", points: 185, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_2.png?raw=true" },
-            { name: "Johnson", points: 160, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_3.png?raw=true" },
-            { name: "Rosey", points: 130, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_4.png?raw=true" },
-            { name: "Scarlett Angela", points: 110, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_5.png?raw=true" }
+            { name: "Charles John", points: 195 },
+            { name: "Alex Mike", points: 185 },
+            { name: "Johnson", points: 160 },
+            { name: "Rosey", points: 130 },
+            { name: "Scarlett Angela", points: 110 }
         ],
         month: [
-            { name: "Alex Mike", points: 1195, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_2.png?raw=true" },
-            { name: "Johnson", points: 1185, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_3.png?raw=true" },
-            { name: "Charles John", points: 1160, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_1.png?raw=true" },
-            { name: "Scarlett Angela", points: 1130, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_5.png?raw=true" },
-            { name: "Rosey", points: 1110, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_4.png?raw=true" },
+            { name: "Alex Mike", points: 1195 },
+            { name: "Johnson", points: 1185 },
+            { name: "Charles John", points: 1160 },
+            { name: "Scarlett Angela", points: 1130 },
+            { name: "Rosey", points: 1110 },
         ],
         year: [
-            { name: "Scarlett Angela", points: 2195, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_5.png?raw=true" },
-            { name: "Rosey", points: 2185, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_4.png?raw=true" },
-            { name: "Johnson", points: 2160, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_3.png?raw=true" },
-            { name: "Charles John", points: 2130, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_1.png?raw=true" },
-            { name: "Alex Mike", points: 2110, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_2.png?raw=true" }
+            { name: "Scarlett Angela", points: 2195 },
+            { name: "Rosey", points: 2185 },
+            { name: "Johnson", points: 2160 },
+            { name: "Charles John", points: 2130 },
+            { name: "Alex Mike", points: 2110 }
         ]
     };
 
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         let borderColor;
-        switch(category) {
+        switch (category) {
             case "today":
                 leaderboardSection.style.backgroundColor = "rgb(238, 238, 122)";
                 borderColor = "rgb(238, 238, 122)";
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 break;
             case "year":
                 leaderboardSection.style.backgroundColor = "rgb(108, 177, 123)";
-                borderColor =  "rgb(108, 177, 123";
+                borderColor = "rgb(108, 177, 123";
                 break;
         }
 
@@ -75,3 +75,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     loadLeaderboard("month");
 });
+
+
+
+
+
+// today: [
+//     { name: "Charles John", points: 195, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_1.png?raw=true" },
+//     { name: "Alex Mike", points: 185, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_2.png?raw=true" },
+//     { name: "Johnson", points: 160, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_3.png?raw=true" },
+//     { name: "Rosey", points: 130, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_4.png?raw=true" },
+//     { name: "Scarlett Angela", points: 110, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_5.png?raw=true" }
+// ],
+// month: [
+//     { name: "Alex Mike", points: 1195, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_2.png?raw=true" },
+//     { name: "Johnson", points: 1185, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_3.png?raw=true" },
+//     { name: "Charles John", points: 1160, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_1.png?raw=true" },
+//     { name: "Scarlett Angela", points: 1130, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_5.png?raw=true" },
+//     { name: "Rosey", points: 1110, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_4.png?raw=true" },
+// ],
+// year: [
+//     { name: "Scarlett Angela", points: 2195, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_5.png?raw=true" },
+//     { name: "Rosey", points: 2185, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_4.png?raw=true" },
+//     { name: "Johnson", points: 2160, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_3.png?raw=true" },
+//     { name: "Charles John", points: 2130, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_1.png?raw=true" },
+//     { name: "Alex Mike", points: 2110, image: "https://github.com/codingmarket07/Leader-Board-/blob/master/pic_2.png?raw=true" }
+// ]
